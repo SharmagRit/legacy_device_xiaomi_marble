382443f1eccfb7888d6ee323a41afc011a4070cc
SharmagRit <sharmagritstuff@gmail.com>
2023-09-10 14:09:21 +0000

    marble: Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,8 +1,12 @@
 echo 'Starting to clone stuffs needed for your device'
+# Nuke stuff
+	rm -rf vendor/xiaomi/miuicamera
+	rm -rf device/xiaomi/marble-kernel
+	rm -rf vendor/xiaomi/marble
 
 echo 'Cloning MiuiCam Tree [1/3]'
-# MiuiCam Tree                                                 git clone https://gitlab.com/SharmagRit/andro>
-	git clone https://git.libremobileos.com/SharmagRit/android_vendor_xiaomi_miuicamera.git vendor/xiaomi/miuicamera
+# MiuiCam Tree
+	git clone https://gitlab.com/SharmagRit/android_vendor_xiaomi_camera.git -b prebuilt vendor/xiaomi/miuicamera
 
 echo 'Cloning Kernel Tree [2/3]'
 # Kernel Tree
@@ -10,6 +14,6 @@ echo 'Cloning Kernel Tree [2/3]'
 
 echo 'Cloning Vendor Tree [3/3]'
 # Vendor Tree
-	git clone https://git.libremobileos.com/SharmagRit/vendor_xiaomi_marble vendor/xiaomi/marble
+	git clone https://gitlab.com/SharmagRit/vendor_xiaomi_marble vendor/xiaomi/marble
 
 echo 'Completed, proceeding to lunch'
