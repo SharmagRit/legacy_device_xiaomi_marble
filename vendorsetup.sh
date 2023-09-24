4fc15f61eb9ea0d4ee51650cfed6e47ad5711891
SharmagRit <sharmagritstuff@gmail.com>
2023-09-24 05:45:46 +0000

    marble: Add `Basic Call Recorder` support

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,15 +1,19 @@
 echo 'Cloning trees for marble'
 
 # Kernel Tree
-echo 'Cloning Kernel Tree [1/3]'
+echo 'Cloning Kernel Tree [1/4]'
 	git clone https://github.com/SharmagRit/android_device_xiaomi_marble-kernel -b udc device/xiaomi/marble-kernel
 
 # Vendor Tree
-echo 'Cloning Vendor Tree [2/3]'
+echo 'Cloning Vendor Tree [2/4]'
 	git clone https://github.com/SharmagRit/android_vendor_xiaomi_marble -b udc vendor/xiaomi/marble
 
 # Miui/Leica Tree
-echo 'Cloning Miui/Leica Tree [3/3]'
+echo 'Cloning Miui/Leica Tree [3/4]'
 	git clone https://gitlab.com/SharmagRit/android_vendor_xiaomi_camera.git -b udc vendor/xiaomi/camera
+
+# BCR tree
+echo 'Cloning BCR Tree [4/4]'
+	git clone https://github.com/SharmagRit/vendor_bcr.git vendor/bcr
 
 echo 'SharmagRit, Cloning process is completed, now its time for lunch'
