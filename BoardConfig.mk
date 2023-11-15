f16547b15ddfb8a68176c768cfce7f280799f4b3
chaitanyakm <chaitanyaarora3@gmail.com>
2023-11-15 21:48:38 +0530

    marble: Import `device_framework_matrix` to Tree

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -241,7 +241,7 @@ DEVICE_MANIFEST_UKEE_FILES := \
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     $(DEVICE_PATH)/configs/vintf/vendor_framework_compatibility_matrix.xml \
     $(DEVICE_PATH)/configs/vintf/xiaomi_framework_compatibility_matrix.xml \
-    vendor/superior/config/device_framework_matrix.xml
+    $(DEVICE_PATH)/configs/vintf/device_framework_matrix.xml
 ODM_MANIFEST_SKUS += marble
 ODM_MANIFEST_MARBLE_FILES := $(DEVICE_PATH)/configs/vintf/manifest_nfc.xml
 
